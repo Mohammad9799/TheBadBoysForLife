a65bdac52bc95db0c1bce596b0040aa0a5c98644
Moe 97 <hmohammad75@yahoo.com>
2020-01-19 17:33:31 -0800

    theFirstCode

---
+++ src/TheFirstProject/FirstPush.java
@@ -0,0 +1,11 @@
+package TheFirstProject;
+
+public class FirstPush {
+    public static void main(String[] args) {
+
+        for (;;){
+            System.out.println("HI");
+        }
+
+    }
+}
